07253cbc3242b317237613112a61becec95d340a
LeSurvivant9 <mcalexandreboisfer@gmail.com>
2025-11-02 16:05:49 +0100

    Update gluetun post-update script to use bash and enhance output messages

--- webhook/scripts/gluetun-post-update.sh
+++ webhook/scripts/gluetun-post-update.sh
@@ -1,12 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 set -e
 
-echo "Webhook reçu : Gluetun a été mis à jour."
+echo "--- Webhook reçu : Gluetun mis à jour  ---"
 
-echo "Redémarrage de qBittorrent..."
-docker restart qbittorrent
+# 1. Re-créer qBittorrent
+echo "Re-création de la stack qbittorrent..."
+docker compose -f /etc/komodo/repos/fight-club/qbittorrent/docker-compose.yml up -d --force-recreate
 
-echo "Redémarrage de nicotine..."
-docker restart nicotine
+# 2. Re-créer Nicotine
+echo "Re-création de la stack nicotine..."
+docker compose -f /etc/komodo/repos/fight-club/nicotine/docker-compose.yml up -d --force-recreate
 
-echo "Conteneurs redémarrés."
+echo "--- Clients VPN mis à jour ---"
