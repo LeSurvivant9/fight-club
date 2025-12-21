6163171a76f5a24d3b8d6342ff1d87fb736791b8
Alexandre BOISFER <mcalexandreboisfer@gmail.com>
2025-12-21 20:16:57 +0100

    feat(docker): add qui service configuration to docker-compose and remove obsolete entries

--- webhook/scripts/autoheal.sh
+++ webhook/scripts/autoheal.sh
@@ -105,7 +105,6 @@ echo "--- Début du traitement Autoheal ---"
 
 # On traite les services spécifiés par l'utilisateur
 process_service "qbittorrent"
-process_service "qui"
 process_service "nicotine"
 process_service "znc"
 
